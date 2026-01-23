dc27f4afc8f29fca209bd2644af540b332f37709
Jaaztillo <jaazaicastillo853@gmail.com>
2026-01-23 14:43:21 -0600

    usless change basically

--- src/main/java/frc/robot/subsystems/Climb_Subsystem.java
+++ src/main/java/frc/robot/subsystems/Climb_Subsystem.java
@@ -6,7 +6,6 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants.ClimbConstants;
 
 // spark imports (SparkMax, SparkMaxConfig, MotorType, PersistMode, ResetMode)
