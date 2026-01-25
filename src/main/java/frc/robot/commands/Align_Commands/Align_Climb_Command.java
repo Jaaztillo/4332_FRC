81827abe1075ca4d2d7f8d20c826ff08d47d60ff
Jaaztillo <jaazaicastillo853@gmail.com>
2026-01-24 19:30:52 -0600

    added safety to the controls

--- src/main/java/frc/robot/commands/Align_Commands/Align_Climb_Command.java
+++ src/main/java/frc/robot/commands/Align_Commands/Align_Climb_Command.java
@@ -10,8 +10,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 
-import com.ctre.phoenix.sensors.PigeonIMU;
-
 import edu.wpi.first.wpilibj.Timer;
 
 import frc.robot.subsystems.Limelight_Subsystem;
